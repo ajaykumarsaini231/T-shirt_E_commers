113b0af51716c2b56b39ca73e78bd863d0e8f2be
“[ajaykumar <[nabalsaini231@gmail.com]>
2025-10-07 07:19:15 +0530

    Fix: removed syntax error in SignInPage.tsx

---
+++ src/app/_zustand/wishlistStore.ts
@@ -0,0 +1,68 @@
+import { create } from "zustand";
+
+export type ProductInWishlist = {
+  id: string;
+  name: string;
+  price: number;
+  image: string;
+};
+
+export type State = {
+  wishlist: ProductInWishlist[];
+  wishQuantity: number;
+};
+
+export type Actions = {
+  addToWishlist: (product: ProductInWishlist) => void;
+  removeFromWishlist: (id: string) => void;
+  setWishlist: (wishlist: ProductInWishlist[]) => void;
+  clearWishlist: () => void;
+};
+
+export const useWishlistStore = create<State & Actions>((set) => ({
+  wishlist: [],
+  wishQuantity: 0,
+
+  addToWishlist: (product) => {
+    set((state) => {
+      const productInWishlist = state.wishlist.find(
+        (item) => product.id === item.id
+      );
+
+      if (!productInWishlist) {
+        return {
+          wishlist: [...state.wishlist, product],
+          wishQuantity: state.wishlist.length + 1,
+        };
+      } else {
+        return {
+          wishlist: [...state.wishlist],
+          wishQuantity: state.wishlist.length,
+        };
+      }
+    });
+  },
+
+  removeFromWishlist: (id) => {
+    set((state) => {
+      const newWishlist = state.wishlist.filter((item) => item.id !== id);
+      return {
+        wishlist: newWishlist,
+        wishQuantity: newWishlist.length,
+      };
+    });
+  },
+
+  setWishlist: (wishlist: ProductInWishlist[]) => {
+    set(() => ({
+      wishlist: [...wishlist],
+      wishQuantity: wishlist.length,
+    }));
+  },
+
+  clearWishlist: () =>
+    set(() => ({
+      wishlist: [],
+      wishQuantity: 0,
+    })),
+}));
